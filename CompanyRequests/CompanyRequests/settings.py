1b0c0aa5dfd515710f1f8123496025bdc9263a02
kidji <kidjimoshi96@gmail.com>
2016-12-11 19:40:29 +0200

    change login and logout settings

--- CompanyRequests/CompanyRequests/settings.py
+++ CompanyRequests/CompanyRequests/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+# urls for auth system
+
+LOGIN_URL = '/login/'
+LOGOUT_URL = '/logout/'
---
+++ CompanyRequests/requests/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from requests import views
+
+urlpatterns = [
+    #url(r'^(?:(?P<id>\d+)/)?$', views.products, name = 'products'),
+    #url(r'^product/(?P<id>\d+)/$', views.product, name = 'product'),
+]
